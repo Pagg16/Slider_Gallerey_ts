5516b4b6be400ac53fc9825c44219f08da77772c
Pavel Kulebin <pagg16pagg@gmail.com>
2024-03-01 15:45:17 +0300

    simple animation

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -6,29 +6,42 @@ import cn from "classnames";
 
 export const Slider: React.FC<SliderProps> = ({ reviews }): JSX.Element => {
   const [currentSlide, setCurrentSlide] = useState<number>(0);
+  const [opacitySlide, setOpacitySlide] = useState<boolean>(false);
 
-  const handleArrow = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    const direction = e.currentTarget.dataset.name;
-    if (direction === "arrow_right") {
-      setCurrentSlide((state) => {
-        if (state + 1 > reviews.length - 1) {
-          return 0;
-        }
-        return state + 1;
-      });
-    } else if (direction === "arrow_left") {
-      setCurrentSlide((state) => {
-        if (state - 1 < 0) {
-          return reviews.length - 1;
-        }
-        return state - 1;
-      });
+  const handleArrow = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    if (opacitySlide) {
+      return;
     }
+    const direction = e.currentTarget.dataset.name;
+    setOpacitySlide(true);
+    setTimeout(() => {
+      setOpacitySlide(false);
+      if (direction === "arrow_right") {
+        setCurrentSlide((state) => {
+          if (state + 1 > reviews.length - 1) {
+            return 0;
+          }
+          return state + 1;
+        });
+      } else if (direction === "arrow_left") {
+        setCurrentSlide((state) => {
+          if (state - 1 < 0) {
+            return reviews.length - 1;
+          }
+          return state - 1;
+        });
+      }
+    }, 300);
   };
 
   return (
     <div className={styles.slider}>
-      <div className={styles.slider__slide}>
+      <div
+        className={cn(
+          styles.slider__slide,
+          opacitySlide && styles.slider__slide__oppasyty
+        )}
+      >
         <div className={styles.slider__description}>
           <div className={styles.slider__title}>
             {reviews[currentSlide].text}
@@ -45,26 +58,29 @@ export const Slider: React.FC<SliderProps> = ({ reviews }): JSX.Element => {
           /> */}
         </div>
       </div>
-      <div
+      <button
         onClick={handleArrow}
         data-name="arrow_right"
-        className={styles.arrow}
+        className={cn(
+          styles.slider__arrow,
+          reviews.length - 1 === currentSlide && styles.slider__arrow_hide
+        )}
       >
         <img className={styles.slider__arrow} src={arrow} alt="control_arrow" />
-      </div>
-      <div
+      </button>
+      <button
         onClick={handleArrow}
         data-name="arrow_left"
-        className={styles.arrow}
+        className={cn(
+          styles.slider__arrow_left,
+          styles.slider__arrow,
+          currentSlide === 0 && styles.slider__arrow_hide
+        )}
       >
-        <img
-          className={cn(styles.slider__arrow_left, styles.slider__arrow)}
-          src={arrow}
-          alt="control_arrow"
-        />
-      </div>
+        <img className={styles.slider__arrow} src={arrow} alt="control_arrow" />
+      </button>
       <div className={styles.slider__counter}>
-        {currentSlide} \ {reviews.length}
+        {currentSlide + 1} \ {reviews.length}
       </div>
     </div>
   );
